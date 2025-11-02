335c205a6c61b9031ff5a35a5be3b027fffb226c
Rooker <rooker@localhost.localdomain>
2025-11-02 21:55:48 +0100

    Bugsy triggers work on all maps

--- Makefile
+++ Makefile
@@ -219,7 +219,7 @@ clean-assets:
 	find $(DATA_ASM_SUBDIR)/maps \( -iname 'connections.inc' -o -iname 'events.inc' -o -iname 'header.inc' \) -exec rm {} +
 
 tidy:
-	$(RM) $(ALL_BUILDS:%=poke%{.gba,.elf,.map,.sav})
+	$(RM) $(ALL_BUILDS:%=poke%{.gba,.elf,.map})
 	$(RM) -r $(BUILD_DIR)
 
 # "friendly" target names for convenience sake
--- include/constants/flags.h
+++ include/constants/flags.h
@@ -189,12 +189,13 @@
 #define FLAG_HIDE_MISC_KANTO_ROCKETS                       0x0AD
 #define FLAG_HIDE_SAFFRON_CITY_POKECENTER_SABRINA_JOURNALS 0x0AE
 #define FLAG_ROUTE22_POLICEMAN_GONE                        0x0AF
+#define FLAG_HIDE_BUGSY_NORTH_GATE                         0x0B0
+#define FLAG_HIDE_BUGSY_VIRIDIAN_FOREST                    0x0B1
+#define FLAG_HIDE_BUGSY_ROUTE2                             0x0B2
+#define FLAG_BUGSY_ITEM_EXCHANGED                          0x0B3
+
 
 // Unused
-#define FLAG_0x0B0               0x0B0
-#define FLAG_0x0B1               0x0B1
-#define FLAG_0x0B2               0x0B2
-#define FLAG_0x0B3               0x0B3
 #define FLAG_0x0B4               0x0B4
 #define FLAG_0x0B5               0x0B5
 #define FLAG_0x0B6               0x0B6
